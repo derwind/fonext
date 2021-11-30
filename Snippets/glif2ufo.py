89460daa3ba6d5af5e2b53a6501d9b7dda3d457b
derwind <uncertainty_principle_ss@yahoo.co.jp>
2021-11-30 23:55:03 +0900

    can control glyphOrder

--- Snippets/glif2ufo.py
+++ Snippets/glif2ufo.py
@@ -9,6 +9,8 @@ def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-o", "--output", dest="out_ufo", metavar="UFO", type=str, required=True,
                         help="output ufo")
+    parser.add_argument("--src_txt_file", dest="src_txt_file", metavar="FILE", type=str, default=None,
+                        help="path to a text file in which unicode values are listed")
     parser.add_argument("in_dir", metavar="DIR", type=str,
                         help="input directory in which .glifs are located")
 
@@ -19,6 +21,11 @@ def get_args():
 def main():
     args = get_args()
 
+    glyphOrder = None
+    if args.src_txt_file:
+        with open(args.src_txt_file) as fin:
+            glyphOrder = [f'uni{int(line.strip(), 16):04X}' for line in fin.readlines()]
+
     uni2glif = {}
     font = Font()
     for file in glob.glob(os.path.join(args.in_dir, '*.glif')):
@@ -28,6 +35,8 @@ def main():
         uni = int(basename.replace('uni', ''), 16)
         uni2glif[basename] = file
         font.newGlyph(basename)
+    if glyphOrder is not None:
+        font.glyphOrder = glyphOrder
     font.save(args.out_ufo)
 
     with open(os.path.join(args.out_ufo, 'glyphs', 'contents.plist'), 'rb') as fin:
